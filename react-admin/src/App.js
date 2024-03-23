337df65b8d5b199fcbe5fc1829e0733f98aa0ef9
Your Name <you@example.com>
2024-03-23 00:13:59 -0300

    habilite secciones en el App.Js desde la consola de mi notebook

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -30,16 +30,16 @@ function App() {
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} /> */}
-              {/* <Route path="/contacts" element={<Contacts />} /> */}
-              {/* <Route path="/invoices" element={<Invoices />} /> */}
-              {/*<Route path="/form" element={<Form />} />*/} No reconoce
-              {/* <Route path="/bar" element={<Bar />} /> */}
-              {/* <Route path="/pie" element={<Pie />} />  */}No reconoce
-              {/* <Route path="/line" element={<Line />} /> */}
-              {/* <Route path="/faq" element={<FAQ />} /> */}
-              {/* <Route path="/calendar" element={<Calendar />} />  */}
-              {/* <Route path="/geography" element={<Geography />} /> */}
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              { /*   <Route path="/form" element={<Form />} /> */} No reconoce
+              <Route path="/bar" element={<Bar />} />
+              { /* <Route path="/pie" element={<Pie />} />  */}No reconoce
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} /> 
+              <Route path="/geography" element={<Geography />} />
             </Routes>
           </main>
         </div>
